d59d2b90cc2274b17d398033a91d483bfd07b922
MilesWright7 <mistermiles777@gmail.com>
2022-07-29 18:13:06 -0700

    upscaling for dall-e images

--- discordbot.py
+++ discordbot.py
@@ -11,6 +11,7 @@ from SpaceManagement import handle_downloads_space
 import requests
 import base64
 import io
+import aiohttp
 
 
 from discord.ext import commands
@@ -393,17 +394,33 @@ async def start_bot_auto_leave():
     return
 
 #region dall-e
-@client.command(help="Pass me text and ill give you images back in about a minute. Uses web api of dall-e mini to generate 9 images based on prompt.\nExample usage: =dalle poker house on fire while homeless dance")
+@client.command(help="Pass me text and ill give you images back in about a minute. Uses web api of dall-e mini to generate 9 images based on prompt.\nExample usage: =dalle poker house on fire while homeless dance", aliases=["da"])
 async def dalle(ctx, *, arg):
-    await ctx.send(embed=discord.Embed.from_dict({"title": "generating images", "description": f"generating images from prompt \"{arg}\".\nThis could take up to 2 minutes."}))
-    r = requests.post("https://backend.craiyon.com/generate", json={"prompt": arg})
-    files = []
-    for img in r.json()['images']:
-        f = discord.File(io.BytesIO(base64.b64decode(img)), filename=f"{arg}.jpeg")
-        files.append(f)
+    async with aiohttp.ClientSession() as session:
+        await ctx.send(embed=discord.Embed.from_dict({"title": "generating images", "description": f"generating images from prompt \"{arg}\".\nThis could take up to 2 minutes."}))
+        async with session.post("https://backend.craiyon.com/generate", json={"prompt": arg}) as r:
+            resp = await r.json()
+            images = resp['images']
+            
+        # upscaling each image
+        tasks = []
+        for img in images:
+            tasks.append(asyncio.ensure_future(upscale(img, session)))
 
-    await ctx.send(files=files)
+        upscaled_images = await asyncio.gather(*tasks)
+
+        files = []
+        for img in upscaled_images:
+            f = discord.File(io.BytesIO(base64.b64decode(img)), filename=f"{arg}.jpeg")
+            files.append(f)
+
+        await ctx.send(files=files)
         
+async def upscale(image_bytes, session):
+    data_string = "data:image//JPEG;base64,"
+    async with session.post("https://upscaler.zyro.com/v1/ai/image-upscaler", json={"image_data": f"{data_string}{image_bytes}"}) as r:
+        resp = await r.json()
+        return resp["upscaled"][len(data_string) - 1:]
 
 #endregion
 #region MTG
