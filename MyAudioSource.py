8d2af804087e6d6f9e3be63327b5fef99e603451
MilesWright7 <mistermiles777@gmail.com>
2022-10-29 01:51:02 -0700

    fix bug with tracking time

--- MyAudioSource.py
+++ MyAudioSource.py
@@ -4,6 +4,7 @@ import discord
 class MyAudioSource(discord.AudioSource):
 	def __init__(self, song, playbackSpeed = 1, nightcore = False):
 		self.song = song
+		song.current_time = 0
 		self.playback_speed = playbackSpeed
 		if nightcore:
 			self.options = "-filter_complex [0:a:0]asetrate=1.15*44.1k,aresample=resampler=soxr:precision=24:osf=s32:tsf=s32p:osr=44.1k"
@@ -15,8 +16,16 @@ class MyAudioSource(discord.AudioSource):
 		
 
 	def read(self):
-		self.song.current_time += .02
-		return self.audio.read()
+		if not self.playing_sound:
+			self.song.current_time += .02
+			return self.audio.read()
+		else:
+			data = self.sound.read()
+			if data:
+				return data
+			else:
+				self.playing_sond = False
+				return self.read()
 
 
 	def cleanup(self):
@@ -31,3 +40,8 @@ class MyAudioSource(discord.AudioSource):
 		# read that amount of time off the front of the file
 		self.audio._stdout.read(discord.opus.Encoder.FRAME_SIZE * seconds * 50)
 		self.song.current_time = seconds
+
+
+	def play_sound(self, sound):
+		self.sound = discord.FFmpegAudio(sound.location())
+		self.playing_sond = True
