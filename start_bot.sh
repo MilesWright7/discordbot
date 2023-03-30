81d55f4f2bc346c6d5721f6ea29ae5002b8b1d96
MilesWright7 <mistermiles777@gmail.com>
2023-03-30 00:52:03 +0000

    perhaps restart works now

--- start_bot.sh
+++ start_bot.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 tmux detach
-tmux kill-server
-tmux new -d -s botsession 'python3.9 VoiceBot.py'
+echo "jumped out of tmux session"
+tmux kill-session -t botsession
+echo "killed server"
+tmux new -d -s "botsession" "python3.9 VoiceBot.py"
+echo "restarted a session"
