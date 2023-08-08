0ad0aa4daf94226495cde914a9219c46c4fc76f5
MilesWright7 <mistermiles777@gmail.com>
2023-08-07 21:07:47 -0700

    trying to fix bot closing itself

--- test.py
+++ test.py
@@ -1,216 +1,76 @@
-from tkinter import Y
-import requests
-import re
 
 
-class MyClass():
-	def __init__(self, x , y):
-		self.x = x
-		self.y = y
+from __future__ import print_function
 
-	def __hash__(self):
-		return hash((self.x, self.y))
+import os.path
 
-	def __eq__(self, other):
-		if isinstance(other, self.__class__):
-			return self.x == other.x and self.y == other.y
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+import discord
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '1JT5zDoT9ind7NOgk_MF4MkI-9R97JKForhYxSEYSKs0'
+SAMPLE_RANGE_NAME = 'Draft!F1'
+
+RUN_THIS = True
+
+CHANNEL_TO_SEND = 978083163249201162;
+PLAYERS_DICT = {"Gal" : 127673716657225728,
+				"Leaf" : 187785792897155072,
+				"James" : 218922505891610624,
+				"Devin" : 116791562322575361,
+				"Vincent" : 221763209479192577,
+				"Benny" : 226603911832666112,
+				"Miles" : 218920440297553920,
+				"Caleb" : 80546742629564416}
+
+
+def main():
+	"""Shows basic usage of the Sheets API.
+	Prints values from a sample spreadsheet.
+	"""
+	creds = None
+	# The file token.json stores the user's access and refresh tokens, and is
+	# created automatically when the authorization flow completes for the first
+	# time.
+	if os.path.exists('token.json'):
+		creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+	# If there are no (valid) credentials available, let the user log in.
+	if not creds or not creds.valid:
+		if creds and creds.expired and creds.refresh_token:
+			creds.refresh(Request())
 		else:
-			return False
+			flow = InstalledAppFlow.from_client_secrets_file(
+				'credentials.json', SCOPES)
+			creds = flow.run_local_server(port=0)
+		# Save the credentials for the next run
+		with open('token.json', 'w') as token:
+			token.write(creds.to_json())
 
-def func(*args, **kwargs):
-	print(f"args = {args}")
-	print(f"kwargs = {kwargs}")
+	try:
+		service = build('sheets', 'v4', credentials=creds)
 
+		# Call the Sheets API
 
-def func2(a, *, arg1=None, arg2=None, arg3=None):
+		# loop this shit to update every hour and notify bullshit andys
+		sheet = service.spreadsheets()
+		result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+									range=SAMPLE_RANGE_NAME).execute()
+		values = result.get('values', [])
+		
+	except HttpError as err:
+		print(err)
+
+def send_nudes(userId, channel : discord.channel.TextChannel):
+	channel.send(f"<@{userId}> its your turn to pick now bruv.\n https://docs.google.com/spreadsheets/d/1JT5zDoT9ind7NOgk_MF4MkI-9R97JKForhYxSEYSKs0/edit#gid=1822506900")
 	
-	print(f"arg1 = {arg1}")
-	print(f"arg2 = {arg2}")
-	print(f"arg3 = {arg3}")
 
 
-# func2(1,arg1="jeff")
-# func(*["one", "two", "three"], epic="fart")
-
-def get_throttling_function_name(js: str) -> str:
-    """Extract the name of the function that computes the throttling parameter.
-
-    :param str js:
-        The contents of the base.js asset file.
-    :rtype: str
-    :returns:
-        The name of the function used to compute the throttling parameter.
-    """
-    function_patterns = [
-        # https://github.com/ytdl-org/youtube-dl/issues/29326#issuecomment-865985377
-        # https://github.com/yt-dlp/yt-dlp/commit/48416bc4a8f1d5ff07d5977659cb8ece7640dcd8
-        # var Bpa = [iha];
-        # ...
-        # a.C && (b = a.get("n")) && (b = Bpa[0](b), a.set("n", b),
-        # Bpa.length || iha("")) }};
-        # In the above case, `iha` is the relevant function name
-        r'a\.[a-zA-Z]\s*&&\s*\([a-z]\s*=\s*a\.get\("n"\)\)\s*&&\s*'
-        r'\([a-z]\s*=\s*([a-zA-Z0-9$]+)(\[\d+\])?\([a-z]\)',
-    ]
-    print('Finding throttling function name')
-    for pattern in function_patterns:
-        regex = re.compile(pattern)
-        function_match = regex.search(js)
-        if function_match:
-            print("finished regex search, matched: %s", pattern)
-            if len(function_match.groups()) == 1:
-                return function_match.group(1)
-            idx = function_match.group(2)
-            if idx:
-                idx = idx.strip("[]")
-                array = re.search(
-                    r'var {nfunc}\s*=\s*(\[.+?\]);'.format(
-                        nfunc=re.escape(function_match.group(1))),
-                    js
-                )
-                if array:
-                    array = array.group(1).strip("[]").split(",")
-                    array = [x.strip() for x in array]
-                    return array[int(idx)]
-
-    # raise RegexMatchError(
-    #     caller="get_throttling_function_name", pattern="multiple"
-    
-
-
-def get_throttling_function_code(js: str) -> str:
-    """Extract the raw code for the throttling function.
-
-    :param str js:
-        The contents of the base.js asset file.
-    :rtype: str
-    :returns:
-        The name of the function used to compute the throttling parameter.
-    """
-    # Begin by extracting the correct function name
-    name = re.escape(get_throttling_function_name(js))
-
-    # Identify where the function is defined
-    pattern_start = r"%s=function\(\w\)" % name
-    regex = re.compile(pattern_start)
-    match = regex.search(js)
-
-    # Extract the code within curly braces for the function itself, and merge any split lines
-    code_lines_list = find_object_from_startpoint(js, match.span()[1]).split('\n')
-    joined_lines = "".join(code_lines_list)
-
-    # Prepend function definition (e.g. `Dea=function(a)`)
-    return match.group(0) + joined_lines
-
-
-
-
-def find_object_from_startpoint(html, start_point):
-    """Parses input html to find the end of a JavaScript object.
-
-    :param str html:
-        HTML to be parsed for an object.
-    :param int start_point:
-        Index of where the object starts.
-    :rtype dict:
-    :returns:
-        A dict created from parsing the object.
-    """
-    html = html[start_point:]
-
-    # First letter MUST be a open brace, so we put that in the stack,
-    # and skip the first character.
-    stack = [html[0]]
-    i = 1
-
-    context_closers = {
-        '{': '}',
-        '[': ']',
-        '"': '"'
-    }
-
-    while i < len(html):
-        if len(stack) == 0:
-            break
-        curr_char = html[i]
-        curr_context = stack[-1]
-
-        # If we've reached a context closer, we can remove an element off the stack
-        if curr_char == context_closers[curr_context]:
-            stack.pop()
-            i += 1
-            continue
-
-        # Strings require special context handling because they can contain
-        #  context openers *and* closers
-        if curr_context == '"':
-            # If there's a backslash in a string, we skip a character
-            if curr_char == '\\':
-                i += 2
-                continue
-        else:
-            # Non-string contexts are when we need to look for context openers.
-            if curr_char in context_closers.keys():
-                stack.append(curr_char)
-
-        i += 1
-
-    full_obj = html[:i]
-    return full_obj  # noqa: R504
-
-
-def get_throttling_plan(js: str):
-    """Extract the "throttling plan".
-
-    The "throttling plan" is a list of tuples used for calling functions
-    in the c array. The first element of the tuple is the index of the
-    function to call, and any remaining elements of the tuple are arguments
-    to pass to that function.
-
-    :param str js:
-        The contents of the base.js asset file.
-    :returns:
-        The full function code for computing the throttlign parameter.
-    """
-    raw_code = get_throttling_function_code(js)
-
-    transform_start = r"try{"
-    plan_regex = re.compile(transform_start)
-    match = plan_regex.search(raw_code)
-
-    transform_plan_raw = js #find_object_from_startpoint(raw_code, match.span()[1] - 1)
-
-    # Steps are either c[x](c[y]) or c[x](c[y],c[z])
-    step_start = r"c\[(\d+)\]\(c\[(\d+)\](,c(\[(\d+)\]))?\)"
-    step_regex = re.compile(step_start)
-    matches = step_regex.findall(transform_plan_raw)
-    transform_steps = []
-    for match in matches:
-        if match[4] != '':
-            transform_steps.append((match[0],match[1],match[4]))
-        else:
-            transform_steps.append((match[0],match[1]))
-
-    return transform_steps
-
-
-text = requests.get('https://www.youtube.com/s/player/59acb1f3/player_ias.vflset/en_US/base.js')
-funcname = get_throttling_plan(text.text)
-print(funcname)
-
-
-'''
-mma=function(a){var b=a.split(""),
-c=[1298660008,
-function(d,e,f,h,l,m){return e(h,l,m)},
-968655468,
-function(){for(var d=64,e=[];++d-e.length-32;)switch(d){case 46:d=95;default:e.push(String.fromCharCode(d));case 94:case 95:case 96:break;case 123:d-=76;case 92:case 93:continue;case 58:d=44;case 91:}return e},
-56115230,
--578061081,
--516346513,
-b,
--1739695292,
--1031455761,
-/[,\]],
-[\]];}
-'''
+if __name__ == '__main__':
+	main()
