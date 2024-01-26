010eca4bef2bbfbfad2a58b28e7acd684f990763
MilesWright7 <mistermiles777@gmail.com>
2024-01-25 20:13:12 -0800

    add gpt stuffs

--- cogs/GPT.py
+++ cogs/GPT.py
@@ -4,38 +4,36 @@ from openai import OpenAI
 
 
 async def setup(bot):
-	await bot.add_cog(Dalle(bot))
+	await bot.add_cog(GPT(bot))
 
 
-class Dalle(commands.Cog):
+class GPT(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 		self.client = OpenAI()
 
 
-	@commands.command(help="Pass me text and ill give you images. Using dalle-2", aliases=["da"])
-	async def dalle(self, ctx, *, arg):
-		await ctx.send(embed=Embed.from_dict({"title": "generating images", "description": f"generating images from prompt \"{arg}\".\nThis could take a bit."}))
+	@commands.command(help="Pass me text and ill be chat gpt", aliases=["gal"])
+	async def gpt(self, ctx, *, arg):
 			
-		await ctx.send(get_images(self.client, arg))
+		await ctx.send(get_text(self.client, arg))
 		
 
-def get_images(client, prompt):
-	response = client.images.generate(
-		model="dall-e-2",
-		prompt=prompt,
-		size="1024x1024",
-		quality="standard",
-		n=1,
-		)
+def get_text(client, prompt):
+	completion = client.chat.completions.create(
+			model="gpt-3.5-turbo",
+			messages=[
+			{"role": "system", "content": "You are a discord moderator and you love anime. You must make fun of a man named Gal who farts and shits a lot."},
+			{"role": "user", "content": prompt}
+			])
 
+	return completion.choices[0].message.content
 
-	return response.data[0].url
 
 
 def main():
 	client = OpenAI()
-	print(get_images(client))
+	print(get_text(client, "bargingo gingite"))
 
 	#completion = client.chat.completions.create(
 	#		model="gpt-3.5-turbo",
